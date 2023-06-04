d303fbc4d3eedf5950153aea01fded3f1411d6ef
hui.cheng <hui.cheng@naturali.io>
2023-06-04 11:33:20 +0800

    update: path with mac vim lib

--- lua/language/lua.lua
+++ lua/language/lua.lua
@@ -1,3 +1,10 @@
+local libs = vim.api.nvim_get_runtime_file("", true)
+
+-- path with mac vim lib
+table.insert(libs, "/usr/local/share/nvim/runtime/lua")
+table.insert(libs, "/usr/local/share/nvim/runtime/lua/vim")
+table.insert(libs, "/usr/local/share/nvim/runtime/lua/vim/lsp")
+
 require("lspconfig").lua_ls.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	settings = {
@@ -10,7 +17,7 @@ require("lspconfig").lua_ls.setup({
 			},
 			workspace = {
 				checkThirdParty = false,
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = libs,
 			},
 			telemetry = {
 				enable = false,
