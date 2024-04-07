eb69ced3a885f05ec0e9f71583987e1559a0fa14
hui cheng <viiacesxh@gmail.com>
2024-04-07 12:06:45 +0800

    update: clipboard

--- init.lua
+++ init.lua
@@ -11,6 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
+vim.opt.clipboard:append("unnamedplus")
+
 vim.cmd([[ let g:ale_linters = {'cpp' : []} ]]) -- fix error on cpp file
 
 require("basic")
